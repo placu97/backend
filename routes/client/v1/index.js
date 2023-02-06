2c021dff8eec8d9b91161b109f390aa844a095e9
placu97 <matteogplacucci@gmail.com>
2023-02-06 11:36:57 +0100

    autosave

---
+++ routes/client/v1/index.js
@@ -0,0 +1,11 @@
+/**
+ * index.js
+ * @description :: index route file of client platform.
+ */
+
+const express =  require('express');
+const router =  express.Router();
+router.use('/client/auth',require('./auth'));
+router.use(require('./userRoutes'));
+
+module.exports = router;
---
+++ routes/client/v1/userRoutes.js
@@ -0,0 +1,29 @@
+/**
+ * userRoutes.js
+ * @description :: CRUD API routes for user
+ */
+
+const express = require('express');
+const router = express.Router();
+const userController = require('../../../controller/client/v1/userController');
+const { PLATFORM } =  require('../../../constants/authConstant'); 
+const auth = require('../../../middleware/auth');
+
+router.route('/client/api/v1/user/me').get(auth(PLATFORM.CLIENT),userController.getLoggedInUserInfo);
+router.route('/client/api/v1/user/create').post(userController.addUser);
+router.route('/client/api/v1/user/addBulk').post(userController.bulkInsertUser);
+router.route('/client/api/v1/user/list').post(userController.findAllUser);
+router.route('/client/api/v1/user/count').post(userController.getUserCount);
+router.route('/client/api/v1/user/:id').get(userController.getUser);
+router.route('/client/api/v1/user/update/:id').put(userController.updateUser);    
+router.route('/client/api/v1/user/partial-update/:id').put(userController.partialUpdateUser);
+router.route('/client/api/v1/user/updateBulk').put(userController.bulkUpdateUser);
+router.route('/client/api/v1/user/softDelete/:id').put(userController.softDeleteUser);
+router.route('/client/api/v1/user/softDeleteMany').put(userController.softDeleteManyUser);
+router.route('/client/api/v1/user/delete/:id').delete(userController.deleteUser);
+router.route('/client/api/v1/user/deleteMany').post(userController.deleteManyUser);
+router.route('/client/api/v1/user/change-password').put(auth(PLATFORM.CLIENT),userController.changePassword);
+router.route('/client/api/v1/user/update-profile').put(auth(PLATFORM.CLIENT),userController.updateProfile);
+router.route('/aaaa').post(userController.aaaa);
+
+module.exports = router;
